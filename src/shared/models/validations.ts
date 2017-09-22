5e489921f247d8f0d96686e96ec0587e2dca5b27
ismael <ismaelnos5@gmail.com>
2017-09-22 08:58:22 -0300

    ajustes

---
+++ src/shared/models/validations.ts
@@ -0,0 +1,12 @@
+
+export class Validation {
+
+    constructor(message:string, success:boolean){
+        this.message = message;
+        this.success = success;
+    }
+
+    message:string;
+    success:boolean;
+
+}
--- src/views/empresa/show/empresa-show.component.ts
+++ src/views/empresa/show/empresa-show.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { DOCUMENT } from "@angular/platform-browser";
 import { EmpresaService } from "../empresa.service";
 import { Empresa } from "../../../shared/models/empresa";
-import { ValidationsMessage } from "../../../shared/models/validations-message";
+import { Validation } from "../../../shared/models/validations";
 import { Guid } from "../../../helper/guid";
 import { Usuario } from "../../../shared/models/usuario";
 
@@ -42,6 +42,8 @@ export class EmpresaShowComponent implements OnInit {
   private dom: Document;
   edit: boolean = false;
   _queryParams: any;
+  mensagens: Array<Validation>;
+
 
 
   constructor(
@@ -79,20 +81,27 @@ export class EmpresaShowComponent implements OnInit {
 
   onSubmit() {
 
-    if(this.beforeSave()) {
+    //if (this.beforeSave()) 
 
-      if (!this.empresa.usuario) {
+    if (this.empresa) {
+      
+            this.empresa.usuario = this.usuario;
+      
+              this.service.save(this.empresa, this._queryParams).subscribe((res) => {
+                if (res) {
+                  this.router.navigate(['empresa']);
+                }
+              });
+            }
+      
 
-      this.empresa.usuario = this.usuario;
-
-      this.service.save(this.empresa, this._queryParams).subscribe((res) => {
-        if (res) {
-          this.router.navigate(['empresa']);
-        }
+    /*if (this.beforeSave()) {
+      
+    }else {
+      this.mensagens.forEach(element => {
+        console.log(element);
       });
-    }
-
-    }
+    }*/
 
     /*if (this.empresa.email) {
       this._message = "Não se preocupe, o usuario e senha já estão sendo enviados ao e-mail que foi informado, '" + this.usuario.email + "'";
@@ -116,61 +125,62 @@ export class EmpresaShowComponent implements OnInit {
 
 
   beforeSave() {
-        
-        let mensagens = new Array<ValidationsMessage>();
 
-        if(!this.empresa.nome){
-          let validation = new ValidationsMessage('Favor inserir um nome', true);
-          mensagens.push(validation);
-        }
-        if(!this.empresa.cnpj){
-         let validation = new ValidationsMessage('Favor inserir um cnpj', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.cidade){
-           let validation = new ValidationsMessage('Favor inserir uma cidade', true);
-          mensagens.push(validation);  
-        }
-        if(!this.empresa.endereco){
-          let validation = new ValidationsMessage('Favor inserir um endereço', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.cep){
-          let validation = new ValidationsMessage('Favor inserir um cep', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.bairro){
-          let validation = new ValidationsMessage('Favor inserir um bairro', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.numero){
-           let validation = new ValidationsMessage('Favor inserir um numero', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.ddd){
-          let validation = new ValidationsMessage('Favor inserir um DDD', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.telefone){
-          let validation = new ValidationsMessage('Favor inserir um Telefone', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.email){
-          let validation = new ValidationsMessage('Favor inserir um email', true);
-          mensagens.push(validation); 
-        }
-        if(!this.empresa.complemento){
-           let validation = new ValidationsMessage('Favor inserir um complemento', true);
-          mensagens.push(validation); 
-        }
+     let mensagens = new Array<Validation>();
 
-        console.log(mensagens);
+    if (!this.empresa.nome) {
+      let validation = new Validation('Favor inserir um nome', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.cnpj) {
+      let validation = new Validation('Favor inserir um cnpj', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.cidade) {
+      let validation = new Validation('Favor inserir uma cidade', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.endereco) {
+      let validation = new Validation('Favor inserir um endereço', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.cep) {
+      let validation = new Validation('Favor inserir um cep', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.bairro) {
+      let validation = new Validation('Favor inserir um bairro', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.numero) {
+      let validation = new Validation('Favor inserir um numero', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.ddd) {
+      let validation = new Validation('Favor inserir um DDD', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.telefone) {
+      let validation = new Validation('Favor inserir um Telefone', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.email) {
+      let validation = new Validation('Favor inserir um email', true);
+      mensagens.push(validation);
+    }
+    if (!this.empresa.complemento) {
+      let validation = new Validation('Favor inserir um complemento', true);
+      mensagens.push(validation);
+    }
 
-        if (mensagens.length > 0) {
-          return false;
-        }else {
-          return true;
-        }
+    console.log(mensagens);
+
+    if (mensagens.length > 0) {
+      this.mensagens = mensagens;
+      return false;
+    } else {
+      return true;
+    }
   }
 
   onCopy(id) {
